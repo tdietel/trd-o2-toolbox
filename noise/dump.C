563a5df80e6b698612bd12e66fbb63a2ea55bdb0
Tom Dietel <tom@dietel.net>
2022-01-18 23:59:41 +0200

    git-annex in xenon

--- noise/dump.C
+++ noise/dump.C
@@ -5,21 +5,22 @@ void dump()
 
     TTree* o2sim;
 
-    auto fdig = new TFile("data/504419/trddigits.root");
-    // auto ftrk = new TFile("data/504419/trdtracklets.root");
-    
-    fdig->GetObject("o2sim", o2sim);
+    auto mainfile = new TFile("data/504419/trddigits.root");
+    mainfile->GetObject("o2sim", o2sim);
     o2sim->AddFriend("o2sim", "data/504419/trdtracklets.root");
 
     TTreeReader* reader = new TTreeReader(o2sim);
-    // hits = new TTreeReaderArray<o2::trd::Hit>(*rdrhits, "TRDHit");
 
     auto trgrecords = new TTreeReaderArray<o2::trd::TriggerRecord>(*reader, "TrackTrg");
     auto digits     = new TTreeReaderArray<o2::trd::Digit>(*reader, "TRDDigit");
-    auto tracklets  = new TTreeReaderArray<o2::trd::Tracklet>(*reader, "Tracklet");
+    // auto tracklets  = new TTreeReaderArray<o2::trd::Tracklet>(*reader, "Tracklet");
 
     while (reader->Next()) {
         cout << "Hello. " << trgrecords->GetSize() << " trigger records" << endl; 
+
+        for (auto rec: trgrecords) {
+            cout << rec << endl;
+        }
 
         // for (int iEvent=0; iEvent<trgrecords->GetSize(); iEvent++) {
         //     auto evrec = trgrecords->At(iEvent);
